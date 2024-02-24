460f16340ae1f522418d0dbf3190222652a22a17
Mikhail <my.rs@mail.ru>
2024-02-24 12:33:10 +0300

    add logs, change .gitignore

--- prj/recruitmentSystem/src/main/java/ru/sfedu/api/HibernateUtil.java
+++ prj/recruitmentSystem/src/main/java/ru/sfedu/api/HibernateUtil.java
@@ -6,26 +6,53 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.hibernate.HibernateException;
+
 public class HibernateUtil {
 
     private static SessionFactory sessionFactory;
     
+    private static final Logger log = LogManager.getLogger(HibernateUtil.class.getName());
     /**
      * Создание фабрики
      *
      */
     public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            // loads configuration and mappings
-            Configuration configuration = new Configuration().configure();
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                            .applySettings(configuration.getProperties()).build();
-            MetadataSources metadataSources = new MetadataSources(serviceRegistry);
-//            metadataSources.addAnnotatedClass(TestEntity.class);// Аннотированная сущность
-            metadataSources.addResource("named-queries.hbm.xml");// Именованные запросы
-            sessionFactory = metadataSources.buildMetadata().buildSessionFactory();
+        log.debug("getSessionFactory [1]: getting session factory");
+        
+        if (sessionFactory == null) {            
+            log.debug("getSessionFactory [2]: load config file (default directory)");
+            
+            try{
+                // loads configuration and mappings
+                Configuration configuration = new Configuration().configure();
+
+                log.debug("getSessionFactory [3]: get ServiceRegistry");
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                                .applySettings(configuration.getProperties()).build();
+
+                log.debug("getSessionFactory [4]: get MetadataSources");
+
+                MetadataSources metadataSources = new MetadataSources(serviceRegistry);
+                //metadataSources.addAnnotatedClass(TestEntity.class);// Аннотированная сущность
+
+                log.debug("getSessionFactory [5]: metadataSources.addResource");
+
+                metadataSources.addResource("named-queries.hbm.xml");// Именованные запросы
+
+                log.debug("getSessionFactory [6]: final getting session factory");
+
+                sessionFactory = metadataSources.buildMetadata().buildSessionFactory();
+            
+            } catch(HibernateException ex){
+                log.error("getSessionFactory [7]: error = {}", ex);
+            }
         }
+        
         return sessionFactory;
     }
 }
-}
+
---
+++ prj/recruitmentSystem/src/test/java/ru/sfedu/api/HibernateUtilTest.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package ru.sfedu.api;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author mike
+ */
+public class HibernateUtilTest {
+    private static final Logger log = LogManager.getLogger(HibernateUtilTest.class.getName());
+    public HibernateUtilTest() {
+    }
+
+    @Test
+    public void testGetSessionFactory() {
+        log.debug("testGetSessionFactory [1]: get instance session factory");
+        
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        
+        log.debug("testGetSessionFactory [2]: open session");
+        
+        Session session = sessionFactory.openSession();
+        
+        log.debug("testGetSessionFactory [3]: close session");
+        
+        sessionFactory.close();
+    }
+    
+}
